e1494d2c513ac4b876ab94901086e13aee66d5de
harsh-sahare <71363992+harsh-sahare@users.noreply.github.com>
2024-08-05 07:23:33 +0530

    Update searchToy.ts

--- src/toy/searchToy.ts
+++ src/toy/searchToy.ts
@@ -2,10 +2,10 @@ import { userCredentials, toyDetails } from "../fieldType";
 import prisma_client from "../prisma_client";
 import validUserUiid from "./utility";
 
-export default function searchToy(toyId: string) {
+export default function searchToy(toyName: string) {
   // Todo for security person check if the owener and currently logined in person is same or not
-  const toy = prisma_client.toy.findUnique({
-    where: { uiid: toyId, deleted: false },
+  const toy = prisma_client.toy.findMany({
+    where: { name: toyName, deleted: false },
   });
   return toy;
 }
