be5eaa73ad0b9f4d690956bb4745bbdc8455268d
harsh-sahare <71363992+harsh-sahare@users.noreply.github.com>
2024-08-05 07:24:08 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import prisma_client from "./prisma_client";
+import deleteToy from "./toy/deleteToy";
+import searchToy from "./toy/searchToy";
+import updateToy from "./toy/updateToy";
+import createNewUser from "./user/createUser";
+import loginUser from "./user/loginUser";
 
 async function main() {
-  console.log("hello");
+  // createNewUser( {email,password} ):
+  // return newly created users {uiid , email}
 
-  // const user = await loginUser({
-  //   email: "harsh@gmail.com",
-  //   password: "harsh123",
-  // });
-  // console.log(user);
-  // if (user != null) {
-  //   console.log(user);
-  //   const toy = await searchToy("494bccd9-d721-4596-97c9-8d9a4cbbfdfc");
-  //   console.log(toy);
-  // }
+  // loginUser( {email,password} ):
+  // return users data {uiid , email , directory }
+
+  // createToy( {uuid , email , directory } , { toyname } )
+  // return newly created toy
+
+  // updateToy(toyId , data)
+  // returns updated toy
+
+  //  deleteToy(toyId)
+  // soft deletes the entry
+  // returns nothing
+  
+  // searchToy(Toyname [case sensitive])
+  // returns array of toys with same same
 }
 
 main()
